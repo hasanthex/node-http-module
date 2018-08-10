45e8e8f4513c29559ca5295e01bbac95065e4032
Muhammad Hasan <hasan18353@gmail.com>
2018-08-10 21:02:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// Load http module
 const http = require('http');
 
-// Create a server
 const server = http.createServer((req, res)=>{
-    // Taking url request.  
     if(req.url === '/'){
         res.write('Hello World');
         res.end();
     }
+
+    if(req.url === '/about'){
+        res.write('This is about content');
+        res.end();
+    }
 });
 
-// Run Server
 server.listen(3000);
-
 console.log('Run Server....');
